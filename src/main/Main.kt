bcd7d090a5fe250d51de193f1b48b2cae71d66b0
BrunoPanizzi <brunopanizziq@gmail.com>
2023-10-11 20:09:40 -0300

    Removed hard-coded day from entry point

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -1,6 +1,9 @@
-fun main() {
-    val day = 13
-
-    println("Running day $day")
-    days.runDay(day)
+fun main(args: Array<String>) {
+    try {
+        val day = args[0].toInt()
+        println("Running day $day")
+        days.runDay(day)
+    } catch (e: Exception) {
+        println("Day to run was not specified.")
+    }
 }
