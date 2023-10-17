71a745e301bd284edb3c70dfeb45071b049ce56d
BrunoPanizzi <brunopanizziq@gmail.com>
2023-10-17 20:13:24 -0300

    Made main safer and better with errors

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -1,9 +1,14 @@
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
+    val day: Int
     try {
-        val day = args[0].toInt()
-        println("Running day $day")
-        days.runDay(day)
+        day = args[0].toInt()
     } catch (e: Exception) {
         println("Day to run was not specified.")
+        exitProcess(1)
     }
+
+    println("Running day $day")
+    days.runDay(day)
 }
